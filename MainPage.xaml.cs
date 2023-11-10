62b820bb387a36284c67a6e042a749a13471d6ba
MartyIX <203266+MartyIX@users.noreply.github.com>
2023-11-10 12:19:35 +0100

    Testing

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,15 +1,47 @@
-﻿namespace MyMauiWebView202311;
+﻿using System;
+using System.Diagnostics;
+
+namespace MyMauiWebView202311;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
-	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+        myWebView.Navigating += WebViewNavigating;
+        myWebView.Navigated += WebViewNavigated;
+
+        Dispatcher.Dispatch(() =>
+        {
+            Debug.WriteLine($"[constructor] Change myWebView.Source for the first time");
+            myWebView.Source = new HtmlWebViewSource() { Html = "<html><body><h1>#1: In constructor</h1></body></html>" };
+        });
+    }
+
+    /// <inheritdoc cref="VisualElement.Loaded"/>
+    private void WebViewLoaded(object sender, EventArgs e)
+    {
+        //Debug.WriteLine($"[WebViewLoaded] Change myWebView.Source for the second time");
+        // myWebView.Source = new UrlWebViewSource() { Url = "https://github.com/dotnet/maui/" };
+        // myWebView.Source = new HtmlWebViewSource() { Html = "<html><body><h1>#2: After loaded</h1></body></html>" };
+    }
+
+    /// <inheritdoc cref="EventHandler{WebNavigatingEventArgs}"/>
+    private void WebViewNavigating(object? sender, WebNavigatingEventArgs e)
+    {
+        Debug.WriteLine($"[WebViewNavigating] * e.URL='{e.Url}'");
+    }
+
+    /// <inheritdoc cref="EventHandler{WebNavigatedEventArgs}"/>
+    private void WebViewNavigated(object? sender, WebNavigatedEventArgs e)
+    {
+        Debug.WriteLine($"[WebViewNavigated] * e.URL='{e.Url}'");
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
 
