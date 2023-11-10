d2a12736dab4bce296c4b366bc033eb7c96a1e6e
MartyIX <203266+MartyIX@users.noreply.github.com>
2023-11-10 11:56:39 +0100

    Clean project

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MyMauiWebView202311;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
